0367e96bec7ceb10cc3f7267bf1c3010e80b86c9
jacob.huang <823771179@qq.com>
2019-12-23 22:46:48 +0800

    add zikpin

--- order/src/main/java/com/jacob/cloud/order/controller/OrderController.java
+++ order/src/main/java/com/jacob/cloud/order/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.jacob.cloud.itemapi.entity.Item;
 import com.jacob.cloud.itemapi.feign.ItemClient;
 import com.jacob.cloud.order.service.OrderService;
 import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
